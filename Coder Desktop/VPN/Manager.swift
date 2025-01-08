ce4f0da122de26d49feadc942e94a660887b0e9d
Ethan Dickson <ethan@coder.com>
2025-01-08 15:44:31 +1100

    flip recv and send

--- Coder Desktop/VPN/Manager.swift
+++ Coder Desktop/VPN/Manager.swift
@@ -7,11 +7,12 @@ actor Manager {
     let downloader: Downloader
 
     var tunnelHandle: TunnelHandle?
-    var speaker: Speaker<Vpn_TunnelMessage, Vpn_ManagerMessage>?
+    var speaker: Speaker<Vpn_ManagerMessage, Vpn_TunnelMessage>?
     // TODO: XPC Speaker
 
     private let dest = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         .first!.appending(path: "coder-vpn.dylib")
+    private let logger = Logger(subsystem: "com.coder.Coder.CoderPacketTunnelProvider", category: "manager")
 
     init(with: PacketTunnelProvider) {
         ptp = with
