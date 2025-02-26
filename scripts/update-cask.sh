3b45c5c3f9081119f7d51114c12455cc9ed7ecc2
Thomas Kosiewski <thoma471@googlemail.com>
2025-02-26 11:44:16 +0100

    feat(scripts): add uninstall and zap stanzas (#77)
    Fixed a typo in the release workflow and added uninstall/zap instructions to the Homebrew cask
    
    Change-Id: I529fc138981af6cad8566d8d5d40f474357d920a
    Signed-off-by: Thomas Kosiewski <tk@coder.com>

--- scripts/update-cask.sh
+++ scripts/update-cask.sh
@@ -106,6 +106,19 @@ cask "coder-desktop${SUFFIX}" do
   depends_on macos: ">= :sonoma"
 
   app "Coder Desktop.app"
+
+  uninstall quit: [
+              "com.coder.Coder-Desktop",
+              "com.coder.Coder-Desktop.VPN",
+            ],
+            login_item: "Coder Desktop"
+
+  zap trash: [
+        "~/Library/Caches/com.coder.Coder-Desktop",
+        "~/Library/HTTPStorages/com.coder.Coder-Desktop",
+        "~/Library/Preferences/com.coder.Coder-Desktop.plist",
+      ],
+      delete: "/var/root/Library/Containers/com.Coder-Desktop.VPN/Data/Documents/coder-vpn.dylib"
 end
 EOF
 
