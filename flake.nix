7a2b0bcf61c2021eb6d70f86b8bb970e6d3fe9e4
Ethan Dickson <ethan@coder.com>
2025-03-07 14:35:57 +1100

    chore: manage mutagen daemon lifecycle

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    grpc-swift.url = "github:i10416/grpc-swift-flake";
   };
 
   outputs =
@@ -11,6 +12,7 @@
       self,
       nixpkgs,
       flake-utils,
+      grpc-swift,
     }:
     flake-utils.lib.eachSystem
       (with flake-utils.lib.system; [
@@ -40,7 +42,8 @@
                 git
                 gnumake
                 protobuf_28
-                protoc-gen-swift
+                grpc-swift.packages.${system}.protoc-gen-grpc-swift
+                grpc-swift.packages.${system}.protoc-gen-swift
                 swiftformat
                 swiftlint
                 xcbeautify
