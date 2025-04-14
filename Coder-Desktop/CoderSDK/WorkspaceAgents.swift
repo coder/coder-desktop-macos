75f17caaac1786cfdca642d2205350a7ea820a7c
Ethan Dickson <ethan@coder.com>
2025-04-14 12:26:46 +1000

    fmt

--- Coder-Desktop/CoderSDK/WorkspaceAgents.swift
+++ Coder-Desktop/CoderSDK/WorkspaceAgents.swift
@@ -1,8 +1,7 @@
 import Foundation
 
-
 public extension Client {
-        func agentConnectionInfoGeneric() async throws(SDKError) -> AgentConnectionInfo {
+    func agentConnectionInfoGeneric() async throws(SDKError) -> AgentConnectionInfo {
         let res = try await request("/api/v2/workspaceagents/connection", method: .get)
         guard res.resp.statusCode == 200 else {
             throw responseAsError(res)
