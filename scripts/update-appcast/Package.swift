0a755095db5bae723f6e04c459b12d504a8954c5
Ethan Dickson <ethan@coder.com>
2025-05-28 20:58:19 +1000

    ci: add update-appcast script

---
+++ scripts/update-appcast/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "update-appcast",
+    platforms: [
+        .macOS(.v15),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "update-appcast", dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]
+        ),
+    ]
+)
