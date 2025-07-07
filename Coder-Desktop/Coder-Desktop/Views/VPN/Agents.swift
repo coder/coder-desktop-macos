374edfb6a96b1703a1deb565c066557bb6c3ec21
Ethan Dickson <ethan@coder.com>
2025-07-07 19:18:43 +1000

    feat: make workspaces list scrollable on overflow

--- Coder-Desktop/Coder-Desktop/Views/VPN/Agents.swift
+++ Coder-Desktop/Coder-Desktop/Views/VPN/Agents.swift
@@ -16,28 +16,32 @@ struct Agents<VPN: VPNService>: View {
             if vpn.state == .connected {
                 let items = vpn.menuState.sorted
                 let visibleItems = viewAll ? items[...] : items.prefix(defaultVisibleRows)
-                ForEach(visibleItems, id: \.id) { agent in
-                    MenuItemView(
-                        item: agent,
-                        baseAccessURL: state.baseAccessURL!,
-                        expandedItem: $expandedItem,
-                        userInteracted: $hasToggledExpansion
-                    )
-                    .padding(.horizontal, Theme.Size.trayMargin)
-                }.onChange(of: visibleItems) {
-                    // If no workspaces are online, we should expand the first one to come online
-                    if visibleItems.filter({ $0.status != .off }).isEmpty {
-                        hasToggledExpansion = false
-                        return
+                ScrollView(showsIndicators: false) {
+                    ForEach(visibleItems, id: \.id) { agent in
+                        MenuItemView(
+                            item: agent,
+                            baseAccessURL: state.baseAccessURL!,
+                            expandedItem: $expandedItem,
+                            userInteracted: $hasToggledExpansion
+                        )
+                        .padding(.horizontal, Theme.Size.trayMargin)
+                    }.onChange(of: visibleItems) {
+                        // If no workspaces are online, we should expand the first one to come online
+                        if visibleItems.filter({ $0.status != .off }).isEmpty {
+                            hasToggledExpansion = false
+                            return
+                        }
+                        if hasToggledExpansion {
+                            return
+                        }
+                        withAnimation(.snappy(duration: Theme.Animation.collapsibleDuration)) {
+                            expandedItem = visibleItems.first?.id
+                        }
+                        hasToggledExpansion = true
                     }
-                    if hasToggledExpansion {
-                        return
-                    }
-                    withAnimation(.snappy(duration: Theme.Animation.collapsibleDuration)) {
-                        expandedItem = visibleItems.first?.id
-                    }
-                    hasToggledExpansion = true
                 }
+                .scrollBounceBehavior(.basedOnSize)
+                .frame(maxHeight: 400)
                 if items.count == 0 {
                     Text("No workspaces!")
                         .font(.body)
