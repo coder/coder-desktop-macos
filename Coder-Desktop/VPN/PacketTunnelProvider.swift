8e18bbf863e18dcb63ce1179c15660df92080266
Ethan Dickson <ethan@coder.com>
2026-03-06 17:34:25 +1100

    Remove nonisolated(nonsending) for CI Xcode compat

--- Coder-Desktop/VPN/PacketTunnelProvider.swift
+++ Coder-Desktop/VPN/PacketTunnelProvider.swift
@@ -43,7 +43,7 @@ class PacketTunnelProvider: NEPacketTunnelProvider, @unchecked Sendable {
         return nil
     }
 
-    nonisolated(nonsending) override func startTunnel(
+    override func startTunnel(
         options _: [String: NSObject]?
     ) async throws {
         globalHelperXPCClient.ptp = self
